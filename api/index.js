4f0741b76cbda9b1c1c6b3621829b01bc25db824
prabhjot2001 <lazycoder001@gmail.com>
2025-02-22 15:24:47 +0530

    database configuration completed

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config"
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((err) => {
+    console.log("error occured while connecting to database -> ", err);
+  });
 
 const app = express();
 
-app.listen(8000, ()=>{
-    console.log("Server is running on port:8000")
-})
+app.listen(8000, () => {
+  console.log("Server is running on port:8000");
+});
