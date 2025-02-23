0532ae7ccf5f94a12fba8b314704376807a8ff87
prabhjot2001 <lazycoder001@gmail.com>
2025-02-23 19:00:40 +0530

    redux store config completed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,14 +5,18 @@ import App from "@/App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import RootLayout from "@/layouts/RootLayout.tsx";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <RootLayout>
-        <App />
-      </RootLayout>
-      <Toaster />
+      <Provider store={store}>
+        <RootLayout>
+          <App />
+        </RootLayout>
+        <Toaster />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
